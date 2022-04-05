b489c17ec914d4e2d2294d5f3af8eac51a9bc384
dimitriearose <60557916+dimitriearose@users.noreply.github.com>
2022-04-04 20:20:06 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+name = input("Please Remind me of my name")
+
+print("Oh Yeah, I am " + name)
+
+print("How can I help you?")
+print("1. Math")
+print("2. Look something up")
+#https://apilist.fun/api/random-facts-api
+print("3. Hey, did you know...")
